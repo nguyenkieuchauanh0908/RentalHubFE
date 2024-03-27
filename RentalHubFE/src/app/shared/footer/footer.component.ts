4a6c2e90f46547a9d33feff80a69cdeb9b563c27
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-03-27 17:03:40 +0700

    [UPDATE] Insert logo for footer

--- RentalHubFE/src/app/shared/footer/footer.component.ts
+++ RentalHubFE/src/app/shared/footer/footer.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  contactPhone = '(+84)0909090909';
+  contactEmail = 'rental-hub@gmail.com';
+  faceBook = '';
+  twitter = '';
 }
