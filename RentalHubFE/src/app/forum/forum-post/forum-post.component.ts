a221769fc00501b0efc671fcc7213a77ebf19817
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-06-21 00:33:17 +0700

    [CREATE] Contact to social post creator

--- RentalHubFE/src/app/forum/forum-post/forum-post.component.ts
+++ RentalHubFE/src/app/forum/forum-post/forum-post.component.ts
@@ -14,6 +14,12 @@ import { AccountService } from 'src/app/accounts/accounts.service';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 import { resDataDTO } from 'src/app/shared/resDataDTO';
 import { ForumService } from '../forum.service';
+import {
+  ChatBotService,
+  UserChatsType,
+} from 'src/app/shared/chat-bot/chat-bot.service';
+import { User } from 'src/app/auth/user.model';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-forum-post',
@@ -28,12 +34,30 @@ export class ForumPostComponent implements OnInit, OnDestroy, AfterViewInit {
   $destroy: Subject<Boolean> = new Subject();
   isAuthenticated: boolean = false;
   seeMore: boolean = false;
+  currentUser: User | null = null;
+  currentChat: UserChatsType | null = null;
 
-  constructor(public dialog: MatDialog, private router: Router) {}
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+    private accountService: AccountService,
+    private chatBotService: ChatBotService,
+    private notifierService: NotifierService
+  ) {}
   ngAfterViewInit(): void {
     this.socialContentToDisplay!.nativeElement.innerHTML = this.post._content;
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.accountService.getCurrentUser
+      .pipe(takeUntil(this.$destroy))
+      .subscribe((user) => {
+        if (user) {
+          this.currentUser = user;
+        } else {
+          this.router.navigate(['/auth/login']);
+        }
+      });
+  }
 
   ngOnDestroy(): void {
     console.log('ngOnDestroy called');
@@ -63,6 +87,44 @@ export class ForumPostComponent implements OnInit, OnDestroy, AfterViewInit {
         .pipe(takeUntil(this.$destroy))
         .subscribe(() => {
           sub.unsubscribe();
+        });
+    }
+  }
+
+  addToContactAndGoToChatBot() {
+    console.log('addToContactAndGoToChatBot');
+    if (this.currentUser && this.post._authorId) {
+      //Thêm chat mới vào chat's list (nếu chưa có trong chat list); update lại currentChat; update lại trạng thái của chatBotMenu, update lại chats hiển thị
+      let updatedChats: UserChatsType[] | null = null;
+      this.chatBotService
+        .createNewChat(this.currentUser._id, this.post._authorId)
+        .pipe(takeUntil(this.$destroy))
+        .subscribe((res) => {
+          if (res.data) {
+            this.chatBotService.setChatBotMenuOpened(true);
+            this.chatBotService.setSeeContactList(false);
+            this.chatBotService.getCurrentUserChats.subscribe((chats) => {
+              if (chats) {
+                updatedChats = chats;
+              }
+            });
+            //Kiểm tra xem chat đã tồn tại chưa, nếu chưa thì cập nhật lại userChats
+            let chatExisted = false;
+            for (let i = 0; i < updatedChats!.length; i++) {
+              if (updatedChats![i]._id === res.data._id) {
+                chatExisted = true;
+                break;
+              }
+            }
+            if (!chatExisted) {
+              this.chatBotService.setCurrentUserChats([
+                res.data,
+                ...updatedChats!,
+              ]);
+            }
+
+            this.chatBotService.setCurrentChat(res.data);
+          }
         });
     }
   }
