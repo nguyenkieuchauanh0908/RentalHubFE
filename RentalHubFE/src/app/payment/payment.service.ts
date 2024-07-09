dbc15a81199a6b85c275f2f139a897827ea78a9f
lamhoangduyen2k2 <lamhoangduyen9a4@gmail.com>
2024-07-09 22:23:22 +0700

    [UPDATE]Payment (Duyen)

--- RentalHubFE/src/app/payment/payment.service.ts
+++ RentalHubFE/src/app/payment/payment.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { resDataDTO } from '../shared/resDataDTO';
 import { environment } from 'src/environments/environment';
+import { catchError, tap } from 'rxjs';
+import { handleError } from '../shared/handle-errors';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +12,21 @@ export class PaymentService {
   constructor(private http: HttpClient) {}
 
   payByVNPay(uId: string, amountInput: number) {
-    return this.http.post(
-      environment.basePaymentAPI + 'order/create_payment_url',
-      {
-        contentPayment: uId,
-        amountInput: amountInput,
-      }
-    );
+    return this.http
+      .post<resDataDTO>(
+        environment.basePaymentAPI + 'order/create_payment_url',
+        {
+          contentPayment: uId,
+          amountInput: amountInput,
+        }
+      )
+      .pipe(
+        catchError(handleError),
+        tap((res) => {
+          console.log('🚀 ~ PaymentService ~ tap ~ res:', res);
+          window.location.href = res.data;
+        })
+      );
   }
 
   getPaymentPackageInfo() {
